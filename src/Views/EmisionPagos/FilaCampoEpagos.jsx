f60b1f6288cb2f26521d8fedead532bfa7043789
SCES\rchinchay <rchinchay@sugit.com.ar>
2021-06-30 16:23:55 -0300

    otro

--- src/Views/EmisionPagos/FilaCampoEpagos.jsx
+++ src/Views/EmisionPagos/FilaCampoEpagos.jsx
@@ -1,67 +1,85 @@
 import React, { useEffect, useState } from 'react'
 import { Row, Col, Input, Form, Popconfirm, Select, InputNumber, Radio } from 'antd'
 import { CloseCircleOutlined } from '@ant-design/icons'
-import { useSelector } from 'react-redux'
-import { toogleSelectedOpcionEpagos, setCorrespondencia, searchInCorrespondencia } from '../../Store/Modules/EmisionPagos/actions'
+// import { useSelector } from 'react-redux'
+// import { toogleSelectedOpcionEpagos, setCorrespondencia, searchInCorrespondencia } from '../../Store/Modules/EmisionPagos/actions'
 
-const FilaCampoEpagos = ({ dato }) => {
+const FilaCampoEpagos = ({ dato, opcionesEpagos, eliminarCampo, asignarOpcionACampo }) => {
   const [opcionesSelect, setOpcionesSelect] = useState([])
 
-  const { opcionesEpagos, arrayCorrespondencia } = useSelector((state) => ({
-    opcionesEpagos: state.datosPago.opcionesEpagos,
-    arrayCorrespondencia: state.datosPago.arrayCorrespondencia,
-  }))
-
   useEffect(() => {
-    setOpcionesSelect(opcionesEpagos
-      .filter(opt => !opt.selected)
-      .map(({pcaID, pcaDescripcion}) => (
-        <Select.Option key={pcaID} value={pcaID}>
-          {pcaDescripcion}
-        </Select.Option>
-      ))
-    )
-    /*
-    setArrayCorrespondencia(opcionesEpagos
-      .map(({pcaID}) => {
-        return { pcaID:}
-      })
-    )
-    */
+    const opciones = opcionesEpagos.map(({camID, pcaDescripcion}) => <Select.Option key={camID} value={camID}>{pcaDescripcion}</Select.Option>)
+    setOpcionesSelect(opciones)
   }, [opcionesEpagos])
+  // const { opcionesEpagos, arrayCorrespondencia } = useSelector((state) => ({
+  //   opcionesEpagos: state.datosPago.opcionesEpagos,
+  //   arrayCorrespondencia: state.datosPago.arrayCorrespondencia,
+  // }))
 
-  const changeOptions = (value, camID) => {
-    // cada vez q se  cambia  el valor de un select voy a guardar en el state un array 
-    // [{camID: 1, value: x}, {camID: 3, value: x}, {camID: 8, value: x}, {camID: 2, value: x}, {camID: 19, value: x}]
-    // de manera que si el valor es undefined y se cambia el selected de true a false
-    if(value) {
-      // se  cambia  el arrayCorrespondencia
-      setCorrespondencia(camID, value)
-      // se cambia a seleccionada la opcion
-      toogleSelectedOpcionEpagos(value)
-    } else {
-      searchInCorrespondencia(camID)
-    }
-  }
+  // useEffect(() => {
+  //   setOpcionesSelect(opcionesEpagos
+  //     .filter(opt => !opt.selected)
+  //     .map(({pcaID, pcaDescripcion}) => (
+  //       <Select.Option key={pcaID} value={pcaID}>
+  //         {pcaDescripcion}
+  //       </Select.Option>
+  //     ))
+  //   )
+  //   /*
+  //   setArrayCorrespondencia(opcionesEpagos
+  //     .map(({pcaID}) => {
+  //       return { pcaID:}
+  //     })
+  //   )
+  //
+  // }, [opcionesEpagos])
 
-  const changeOption = (value, camID) => {
-    value && console.log(`Valor: ${value}, camID : ${camID}`)
-    setCorrespondencia(camID, value)
-  }
+  // const changeOptions = (value, camID) => {
+  //   // cada vez q se  cambia  el valor de un select voy a guardar en el state un array 
+  //   // [{camID: 1, value: x}, {camID: 3, value: x}, {camID: 8, value: x}, {camID: 2, value: x}, {camID: 19, value: x}]
+  //   // de manera que si el valor es undefined y se cambia el selected de true a false
+  //   if(value) {
+  //     // se  cambia  el arrayCorrespondencia
+  //     setCorrespondencia(camID, value)
+  //     // se cambia a seleccionada la opcion
+  //     toogleSelectedOpcionEpagos(value)
+  //   } else {
+  //     searchInCorrespondencia(camID)
+  //   }
+  // }
 
-  const {camDescripcion, camNombre, camObligatorio, camTipo, camID} = dato
-  const tiposDeCampos = {
-    INTEGER : <InputNumber style={{ width: '100%' }} min={1} onChange={e => changeOption(e, camID)}></InputNumber>,
-    VARCHAR: <Select style={{ width: '100%' }} name={camID} placeholder="Elija un valor" onChange={e => changeOptions(e, camID)} rules={[{ required: true, message: 'Complete este campo para continuar'}]}>{opcionesSelect}</Select>,
-    BOOLEAN: <Radio.Group
-        // onChange={e => changeOption(e, camNombre)}
-        // onChange={onChange}
-        // value={value}
-      >
-      <Radio value={1}>Si</Radio>
-      <Radio value={0}>No</Radio>
-    </Radio.Group>
-  }
+  // const changeOption = (value, camID) => {
+  //   value && console.log(`Valor: ${value}, camID : ${camID}`)
+  //   setCorrespondencia(camID, value)
+  // }
+  const { camDescripcion, camNombre, camObligatorio, camTipo, camID, pcaID } = dato
+
+  const INTEGER = <InputNumber
+                    style={{ width: '100%' }}
+                    min={1}
+                    // onChange={e => changeOption(e, camID)}
+                  >
+                  </InputNumber>
+  const VARCHAR = <Select
+                    style={{ width: '100%' }}
+                    name={camID}
+                    placeholder="Elija un valor"
+                    // onChange={e => changeOptions(e, camID)}
+                    // onChange={(e) => handleChangeSelect(e, pcaID)}
+                    onChange={(e) => asignarOpcionACampo(pcaID, e)}
+                    rules={[{ required: true, message: 'Complete este campo para continuar'}]}
+                  >
+                    {opcionesSelect}
+                  </Select>
+  const BOOLEAN = <Radio.Group
+                    // onChange={e => changeOption(e, camNombre)}
+                    // onChange={onChange}
+                    // value={value}
+                  >
+                    <Radio value={1}>Si</Radio>
+                    <Radio value={0}>No</Radio>
+                  </Radio.Group>
+  const tiposDeCampos = { INTEGER, VARCHAR, BOOLEAN }
   
   return (
     <>
@@ -81,7 +99,7 @@ const FilaCampoEpagos = ({ dato }) => {
         <Col md={3} className="margin-top-1">
           {camObligatorio==='N' && <div className="btn-quitar-fila">
             <Popconfirm title="¿Querés eliminar este campo?" okText="Si" cancelText="No"
-              // onConfirm={() => quitarParametroEmision(campo)}
+              onConfirm={() => eliminarCampo(pcaID)}
             >
               <CloseCircleOutlined />
             </Popconfirm>
--- src/Views/EmisionPagos/Paso2.jsx
+++ src/Views/EmisionPagos/Paso2.jsx
@@ -2,13 +2,70 @@ import { useSelector } from 'react-redux'
 import { Typography, Button } from 'antd'
 // import { setOpcionesEpagos } from '../../Store/Modules/EmisionPagos/actions'
 import FilaCampoEpagos from './FilaCampoEpagos'
+import { useEffect, useState } from 'react'
 
 export const Paso2 = ({ cambiarPaso }) => {
-  // const [seleccionados, setSeleccionados] = useState([])
+  // const [opcionesSeleccionables, setOpcionesSeleccionables] = useState([])
+  const [ campos, setCampos ] = useState([])
+  const [ opciones, setOpciones ] = useState([])
   const { camposEpagos, opcionesEpagos } = useSelector((state) => ({
     camposEpagos: state.datosPago.camposEpagos,
     opcionesEpagos: state.datosPago.opcionesEpagos,
   }))
+
+  useEffect(() => {
+    const campos = camposEpagos.map(item => ({
+      ...item,
+      eliminado: false,
+      campoAsignado: null
+    }))
+    setCampos(campos)
+  }, [camposEpagos])
+
+  useEffect(() => {
+    setOpciones(opcionesEpagos)
+  }, [opcionesEpagos])
+
+  const eliminarCampo = (pcaID) => {
+    liberarOpcionEpagos(pcaID)
+    const nuevosCampos = campos.filter(item => item.pcaID !== pcaID)
+    setCampos(nuevosCampos)
+  }
+
+  const liberarOpcionEpagos = (pcaID) => {
+    campos.forEach(item => {
+      let camID = null
+      if(item.pcaID===pcaID && item.campoAsignado) {
+        camID = item.campoAsignado
+        console.log(camID)
+      }
+    })
+    console.log("El campo no tiene asignado un campo de Epagos")
+    
+    // aqui debo volver agregar esa opcion a las opciones disponibles para elegir (opcionesSeleccionables)
+  }
+  const asignarOpcionACampo = (pcaID, camID) => {
+    campos.forEach(campo => {
+      if(campo.pcaID === pcaID) {
+        console.log("Se asigno al campo con pcaID :" + pcaID + " la opcion con camID :" + camID)
+        campo.campoAsignado = camID
+      }
+    })
+    console.log(campos)
+    
+    opciones.forEach(opcion => {
+      // marco como seleccionada la opcion
+      if(opcion.camID === camID) {
+        console.log("Se marco la opcion con camID :" + camID + " como seleccionada")
+        opcion.selected = true
+      }
+    })
+    console.log(opciones)
+
+    // filtro las opciones marcando en true la opcion de acuerdo al camID que pase
+    const opcionesFiltered = opciones.filter(item => !item.selected)
+    setOpciones(opcionesFiltered)
+  }
 
   return (
     <>
@@ -16,7 +73,7 @@ export const Paso2 = ({ cambiarPaso }) => {
         <Typography.Text strong>Parámetros de la Emisión</Typography.Text>
       </div>
       <div>
-        { camposEpagos.map((item, index) => <FilaCampoEpagos key={index} dato={item} opcionesEpagos={opcionesEpagos} />) }
+        { campos.filter(item => !item.eliminado).map((item, index) => <FilaCampoEpagos key={index} dato={item} opcionesEpagos={opciones} eliminarCampo={eliminarCampo} asignarOpcionACampo={asignarOpcionACampo} />) }
       </div>
       <div className="margin-top-6">
         <Button style={{ marginRight: "12px" }} onClick={() => cambiarPaso(-1)}>
